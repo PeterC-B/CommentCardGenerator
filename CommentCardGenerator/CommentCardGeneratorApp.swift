8202f012c7aa8d127fc50c22ed32d4a3fe5f0c53
Peter Compton-Burnett <compton-burnett.p@etoncollege.org.uk>
2022-02-02 11:07:37 +0000

    Initial Commit

---
+++ CommentCardGenerator/CommentCardGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardGeneratorApp.swift
+//  CommentCardGenerator
+//
+//  Created by Compton-Burnett, Peter (PGW) on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
